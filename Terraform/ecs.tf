d4eb30e25bb6d1d553a50819e6032a2435363d38
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-12-16 00:30:54 +0530

    Update ecs.tf

--- Terraform/ecs.tf
+++ Terraform/ecs.tf
@@ -1,24 +1,27 @@
-# ECS Task Definition (FINAL – PRODUCTION SAFE)
+# ECS Cluster
+resource "aws_ecs_cluster" "khaleel_strapi_cluster" {
+  name = "khaleel-strapi-cluster"
+}
+
+# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-
-  cpu    = "256"
-  memory = "512"
+  cpu                      = "256"
+  memory                   = "512"
 
   execution_role_arn = data.aws_iam_role.ecs_execution.arn
 
   container_definitions = jsonencode([
     {
-      name  = "strapi"
-      image = var.image_uri
+      name      = "strapi"
+      image     = var.image_uri
       essential = true
 
       portMappings = [
         {
           containerPort = 1337
-          hostPort      = 1337
           protocol      = "tcp"
         }
       ]
@@ -27,8 +30,6 @@ resource "aws_ecs_task_definition" "strapi_task" {
         { name = "NODE_ENV", value = "production" },
         { name = "HOST", value = "0.0.0.0" },
         { name = "PORT", value = "1337" },
-
-        # REQUIRED STRAPI SECRETS (dummy but valid)
         { name = "APP_KEYS", value = "key1,key2,key3,key4" },
         { name = "API_TOKEN_SALT", value = "randomsalt123" },
         { name = "ADMIN_JWT_SECRET", value = "adminjwtsecret123" },
@@ -38,11 +39,26 @@ resource "aws_ecs_task_definition" "strapi_task" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = "/ecs/strapi"
+          awslogs-group         = aws_cloudwatch_log_group.strapi.name
           awslogs-region        = "ap-south-1"
           awslogs-stream-prefix = "ecs"
         }
       }
     }
   ])
+}
+
+# ECS Service
+resource "aws_ecs_service" "khaleel_strapi_service" {
+  name            = "khaleel-strapi-service"
+  cluster         = aws_ecs_cluster.khaleel_strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.strapi_sg.id]
+    assign_public_ip = true
+  }
 }
