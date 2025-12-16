58fdeb28be5a947c5d0a23a8a5dc4d6969a56f66
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-12-16 11:48:28 +0530

    Update iam.tf

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -1,19 +1,4 @@
-resource "aws_iam_role" "ecs_execution" {
+# Use existing ECS execution role (must already exist in AWS account)
+data "aws_iam_role" "ecs_execution" {
   name = "khaleel-ecs-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
-  role       = aws_iam_role.ecs_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
