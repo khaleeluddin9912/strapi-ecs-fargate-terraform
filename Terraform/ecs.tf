d4a92f82c53615295f4e499f3140a959a2474960
khaleeluddin9912 <khaleeluddin9912@outlook.com>
2025-12-16 09:18:20 +0530

    update

--- Terraform/ecs.tf
+++ Terraform/ecs.tf
@@ -3,26 +3,49 @@ resource "aws_ecs_cluster" "khaleel_strapi_cluster" {
   name = "khaleel-strapi-cluster"
 }
 
-# ECS Task Definition (Fargate Free-Tier Safe)
+# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
 
-  cpu    = "256"
-  memory = "512"
-
-  # Use the existing IAM role
   execution_role_arn = data.aws_iam_role.ecs_execution.arn
 
-  container_definitions = jsonencode([{
-    name  = "strapi"
-    image = var.image_uri
-    portMappings = [{
-      containerPort = 1337
-      protocol      = "tcp"
-    }]
-  }])
+  container_definitions = jsonencode([
+    {
+      name      = "strapi"
+      image     = var.image_uri
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 1337
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [
+        { name = "NODE_ENV", value = "production" },
+        { name = "HOST", value = "0.0.0.0" },
+        { name = "PORT", value = "1337" },
+        { name = "APP_KEYS", value = "key1,key2,key3,key4" },
+        { name = "API_TOKEN_SALT", value = "randomsalt123" },
+        { name = "ADMIN_JWT_SECRET", value = "adminjwtsecret123" },
+        { name = "JWT_SECRET", value = "jwtsecret123" }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.strapi.name
+          awslogs-region        = "ap-south-1"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
 }
 
 # ECS Service
@@ -38,8 +61,4 @@ resource "aws_ecs_service" "khaleel_strapi_service" {
     security_groups  = [aws_security_group.strapi_sg.id]
     assign_public_ip = true
   }
-
-  depends_on = [
-    aws_ecs_task_definition.strapi_task
-  ]
 }
