9f6b8165a279999ef30ed208392ccb938888dd31
khaleeluddin9912 <khaleeluddin9912@outlook.com>
2025-12-22 13:17:07 +0530

    update code

--- Terraform/codedeploy.tf
+++ Terraform/codedeploy.tf
@@ -1,5 +1,5 @@
 resource "aws_codedeploy_app" "strapi_app" {
-  name = "khaleel-strapi-app"
+  name             = "khaleel-strapi-app"
   compute_platform = "ECS"
 }
 
@@ -7,6 +7,7 @@ resource "aws_codedeploy_deployment_group" "strapi_deployment_group" {
   app_name              = aws_codedeploy_app.strapi_app.name
   deployment_group_name = "khaleel-strapi-dg"
   service_role_arn      = data.aws_iam_role.ecs_execution.arn
+
   deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
 
   ecs_service {
@@ -19,21 +20,39 @@ resource "aws_codedeploy_deployment_group" "strapi_deployment_group" {
       action                          = "TERMINATE"
       termination_wait_time_in_minutes = 5
     }
-    deployment_ready_option { action_on_timeout = "CONTINUE_DEPLOYMENT" }
-    green_fleet_provisioning_option { action = "DISCOVER_EXISTING" }
+
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    green_fleet_provisioning_option {
+      action = "DISCOVER_EXISTING"
+    }
   }
 
   load_balancer_info {
     target_group_pair_info {
-      target_groups { name = aws_lb_target_group.strapi_blue.arn }
-      target_groups { name = aws_lb_target_group.strapi_green.arn }
-      prod_traffic_route { listener_arns = [aws_lb_listener.http.arn] }
+      target_group {
+        name = aws_lb_target_group.strapi_blue.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.strapi_green.name
+      }
+
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.http.arn]
+      }
     }
   }
 
   auto_rollback_configuration {
     enabled = true
-    events  = ["DEPLOYMENT_FAILURE","DEPLOYMENT_STOP_ON_ALARM","DEPLOYMENT_STOP_ON_REQUEST"]
+    events  = [
+      "DEPLOYMENT_FAILURE",
+      "DEPLOYMENT_STOP_ON_ALARM",
+      "DEPLOYMENT_STOP_ON_REQUEST"
+    ]
   }
 
   depends_on = [
