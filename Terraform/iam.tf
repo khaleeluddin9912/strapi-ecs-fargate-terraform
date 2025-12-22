82bdcedf1f57cc1295bc36a25f4ca2a6e4e79ca5
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-12-22 18:03:02 +0530

    Update iam.tf

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -1,29 +1,15 @@
-#################################
-# ECS Execution Role
-#################################
+# ECS Execution Role - IMPORT or use data
 data "aws_iam_role" "ecs_execution" {
   name = "khaleel-ecs-execution-role"
 }
 
-#################################
-# CodeDeploy Service Role
-#################################
-# CodeDeploy Service Role
-resource "aws_iam_role" "codedeploy_role" {
+# CodeDeploy Service Role - USE DATA BLOCK
+data "aws_iam_role" "codedeploy_role" {
   name = "khaleel-codedeploy-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "codedeploy.amazonaws.com"
-      }
-    }]
-  })
 }
 
+# Then attach policies to the existing role
 resource "aws_iam_role_policy_attachment" "codedeploy_ecs_policy" {
-  role       = aws_iam_role.codedeploy_role.name
+  role       = data.aws_iam_role.codedeploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
