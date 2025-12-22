cd68d5db10af1845d9c28b81258db4c83243ef0d
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-12-22 17:31:53 +0530

    Update iam.tf

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -8,6 +8,22 @@ data "aws_iam_role" "ecs_execution" {
 #################################
 # CodeDeploy Service Role
 #################################
-data "aws_iam_role" "codedeploy_role" {
+# CodeDeploy Service Role
+resource "aws_iam_role" "codedeploy_role" {
   name = "khaleel-codedeploy-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_ecs_policy" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
